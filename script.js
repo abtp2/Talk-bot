2f3494d0ee14f6ba540622e15ee614712ca2e32b
Ashutosh Pandey <94055977+abtp2@users.noreply.github.com>
2022-09-11 23:55:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+var a = localStorage.getItem("name");
+
+if(a !== null) {
+document.getElementById("form-div").style.display ="none";				
+}
+else{
+document.getElementById("form-div").style.display ="flex";				
+}		
+
+document.getElementsByTagName("footer")[0].onclick = nut.click();
+}
+
+
+document.getElementsByTagName("form")[0].onsubmit = function(){
+var x = document.getElementById("uname");
+var y = document.getElementById("uemail");
+var z = document.getElementById("uqual");
+
+
+event.preventDefault();
+document.getElementById("form-div").style.bottom ="500%";
+localStorage.setItem("name", x.value);
+localStorage.setItem("email", y.value);
+localStorage.setItem("qual", z.value);
+}
